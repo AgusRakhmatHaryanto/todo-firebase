dca8469e530bc396cd23c780820d50dd9c2195c8
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmail.com>
2025-05-21 13:30:14 +0700

    [agus]fix: update firebase settings

--- src/app/lib/utils/firebaseConfig.js
+++ src/app/lib/utils/firebaseConfig.js
@@ -1,19 +1,17 @@
-
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBtMMcvGCdr4NuZRPbVK8hSBAUxF9hKzA",
-  authDomain: "todo-2c866.firebaseapp.com",
-  projectId: "todo-2c866",
-  storageBucket: "todo-2c866.firebasestorage.app",
-  messagingSenderId: "1008654072971",
-  appId: "1:1008654072971:web:d85eea5a98b2d55436e88e",
-  measurementId: "G-1E4BD57P9F",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export  {db}
+export { db };
